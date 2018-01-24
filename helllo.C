277b5471ebda827f6e1955e2066352314b80ad26
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-01-24 16:31:02 +0530

    hello.C

---
+++ helllo.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+    int count, n;
+    printf("\nEnter the number:");
+    scanf("%d",&n);
+    for(count=1;count<=n;count++)
+        {
+            printf(" hello\n");
+        }
+getch();
+}
+
