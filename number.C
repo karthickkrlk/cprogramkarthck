78075f1af72a3082258c95945a271acd05d5f9cd
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-01-31 16:02:45 +0530

    number.C

---
+++ number.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int c,i;
+    printf("enter the number");
+    scanf("%d",&c);
+    for(i=0;a!=0;i++)
+    {
+        c/=10;   
+    }
+        printf("the digits of given numer is %d",i);
+}
