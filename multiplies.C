e50a2d5ed227a5fd0a54a4c32281e5b77e96debc
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-01-31 16:35:23 +0530

    multiplies.c

---
+++ multiplies.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,num,result;
+printf("\n enter the number");
+scanf("%d",&num);
+for(i=0;i<=5;i++)
+{
+result=result*i;
+}
+printf("\n ressult is");
+return 0;
+}
