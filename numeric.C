619c9ba4b32506bc2f1145aba658929334082bc7
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-02-02 16:27:17 +0530

    numeric.c

---
+++ numeric.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n==0,i,flag==0;
+char s[20]
+clrscr;
+printf("\nEnter the string:");
+scanf("%s",&n);
+for(i=0;s[i]=!0;i++);
+{
+if(s[i]=='0'||s[i]=='1'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9')
+{
+flag=falg+1
+}
+}
+if(flag==0)
+{
+printf("\n The given string not contains constant");
+}
+else
+{
+printf("\n The given string contains constant");
+}
+return(0);
+}
+
