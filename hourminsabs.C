a5deb02aa26d7a89d1ca8cb20f45e6132e02de29
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-02-05 16:00:19 +0530

    hourminsabs.c

---
+++ hourminsabs.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int h1,m1,h2,m2,h,m;
+    scanf("%d %d",&h1,&m1);
+    scanf("%d %d",&h2,&m2);
+    h=h1-h2;
+    m=m1-m2;
+    printf("%d %d",h,m);
+}
+getch();
+}
