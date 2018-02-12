c91c96d132160977d656ab535d7a6d892f97e9a0
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-02-12 16:18:11 +0530

    space.C

---
+++ space.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() 
+{
+    int c, S;
+    ns = 0;
+    while ((c = getchar()) != EOF)
+    {
+      
+        if (c == ' ')
+            ++s;
+    }
+    printf("Blanks: %d\n",s);
+    return 0;
+}
