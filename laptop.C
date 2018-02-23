9cde17e49ed14794b96556e7d11d3e5c2b387a7d
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-02-23 16:26:17 +0530

    Create laptop.C

---
+++ laptop.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char str[1000];
+int i,a;
+printf("\n Enter the string:");
+gets(str);
+printf("\n Enter the range:");
+scanf("%d",&a);
+for(i=1;i<=a;i++)
+{
+printf(" %s\n",str);
+}
+getch();
+}
