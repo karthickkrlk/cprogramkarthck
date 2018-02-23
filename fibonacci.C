d3bf32e35779a554c57a3e97aae24cd3c3da974b
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-02-23 15:54:47 +0530

    Create fibonacci.C

---
+++ fibonacci.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ void main()
+{
+	int a,b1=0,b2=1,b3,i;
+	printf("\nEnter the range of sequence :");
+	scanf("%d",&a);
+	printf("\nFibonacci series:");
+	for(i=0;i<=a;i++)
+	{
+	printf("\n %d",b1);
+	b3=b1+b2;
+	b1=b2;
+	b2=b3;
+	
+}
+getch();
+}
