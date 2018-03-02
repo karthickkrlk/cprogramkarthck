186df278e54a76142f071463b3e7ac72e648253b
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-03-02 03:08:57 -0800

    Create rangenotrange.C

---
+++ rangenotrange.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("\nEnter the number:");
+scanf("%d",&a);
+if(a<=10)
+{
+printf("\nIt is in the range:-");
+}
+else
+{
+printf("\n Not in range:-");
+}
+return 0;
+}
