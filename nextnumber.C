a1cb92f3e94bca23cff17b99e2f2dc176baa9a25
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-03-05 16:16:20 +0530

    Create nextnumber.C

---
+++ nextnumber.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+int a;
+clrscr();
+printf("\n Enter the number to be performed:");
+scanf("%d",&a);
+{
+a=a+1;
+}
+printf("\n Number of the integers are:%d",a);
+getch ();
+}
