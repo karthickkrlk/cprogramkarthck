8601f1e22cb36e32d273f1941a7fd1fea8b5c885
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-03-09 03:16:10 -0800

    Create swappping.C

---
+++ swappping.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+int a,b;
+clrscr();
+printf("\n Enter the number to be swapped");
+scanf("%d%d",&a,&b);
+printf("\n Swapped number are:%d\t%d",b,a);
+getch ();
+}
