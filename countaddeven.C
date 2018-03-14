73f74030e68b20266aa3083604857b5a52d2c5d5
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-03-14 16:52:47 +0530

    Create countaddeven.C

---
+++ countaddeven.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\nEnter number to be added:");
+scanf("%d%d",&a,&b);
+{
+c=a+b;
+}
+if(c%2==0)
+{
+printf("\nThe product is even");
+}
+else
+{
+printf("\nThe product is odd");
+}
+getch ();
+}
