4a2c02d7b999c025e14576625e8ea9c909c7ca96
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-03-28 15:58:34 +0530

    Create divisor7.C

---
+++ divisor7.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("\nEnter the number :");
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("\n the given number is  multiplicant of 7");
+}
+else
+{
+printf("\n the given number is not  multiplicant of 7");
+}
+getch();
+}
