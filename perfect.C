8192bc220f9d4e4b9f547dfa7e7590db7994c974
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-03-28 16:39:05 +0530

    Create perfect.C

---
+++ perfect.C
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n enter the number :");
+scanf("%d%d",&a,&b);
+{
+c=a*b;
+}
+if((c/a)==b)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
