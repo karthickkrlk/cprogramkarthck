30a2a2d00ba7f35a2831f4fdc68a2ea70d68900a
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-03-28 16:07:09 +0530

    Create difference.C

---
+++ difference.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n Enter the number:");
+scanf("%d%d",&a,&b);
+{
+c=a-b;
+}
+if(c%2==0)
+{
+printf("\n Difference is even");
+}
+else
+{
+printf("\n difference is odd");
+}
+getch();
+}
